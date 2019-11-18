341a5264cc646ac7d259b693680cb9af4d3f08d4
Jose Avila <javil@ksu.edu>
2019-11-18 15:19:08 -0600

    It actually works.

--- Movies/MovieDatabase.cs
+++ Movies/MovieDatabase.cs
@@ -27,5 +27,42 @@ namespace Movies
         }
 
         public List<Movie> All { get { return movies; } }
+
+        public List<Movie> SearchAndFilter(string searchString, List<string> rating)
+        {
+            //case 0: No search string, no ratings
+            if (searchString == null && rating.Count == 0) return All;
+
+            List<Movie> results = new List<Movie>();
+            foreach(Movie movie in movies)
+            {
+                //case 1: search string AND ratings
+                if (searchString != null && rating.Count > 0)
+                {
+                    if(movie.Title != null && movie.Title != null && movie.Title.Contains(searchString, StringComparison.InvariantCultureIgnoreCase))
+                    {
+                        results.Add(movie);
+                    }
+                }
+                //Case 1: Search string only
+                else if (searchString != null)
+                {
+                    if (movie.Title != null && movie.Title.Contains(searchString, StringComparison.InvariantCultureIgnoreCase))
+                    {
+                        results.Add(movie);
+                    }
+                }
+                // Case 3: Ratings only
+                else if(rating.Count > 0)
+                {
+                    if (rating.Contains(movie.MPAA_Rating))
+                    {
+                        results.Add(movie);
+                    }
+
+                }
+            }
+            return results;
+        }
     }
 }
